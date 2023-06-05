9b06da7fe6addf49a78a6ae22927571a23aae9e1
Mudassar Amin <mudasiramin456@gmail.com>
2023-06-05 09:57:56 +0500

    edit comment

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
-# Create your views here.
+# Create your 
 def home(request):
     Categories = category.objects.all()
     Services = services.objects.all()
