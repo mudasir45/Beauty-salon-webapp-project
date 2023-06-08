364108896fa0cc3fcf42db4e7bde0272a498197c
Mudassar Amin <mudasiramin456@gmail.com>
2023-06-08 16:44:52 +0500

    registration fome done

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_login, name='user_login' )
+    path('userLogin/', views.user_login, name='user_login' ),
+    path('userSignUp/', views.user_SignUp, name='user_SignUp' ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+from main.models import *
 
 # Create your views here.
 def user_login(request):
-    return HttpResponse('user login page')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successfull")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect(user_login)
+    return render(request, 'index.html')
+
+def user_SignUp(request):
+    if request.method == "POST":
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+        email = request.POST.get('email')
+        EmpPhone = request.POST.get('EmpPhone')
+        EmpCNIC = request.POST.get('EmpCNIC')
+        EmpAddress = request.POST.get('EmpAddress')
+        status = request.POST.get('status')
+
+        user = User.objects.create_user(
+            first_name = fname,
+            last_name = lname,
+            email = email, 
+            username = username, 
+            password = pass1
+        )
+        user_porfile = profile.objects.create(
+            user = user,
+            CNIC = EmpCNIC,
+            phone = EmpPhone,
+            adress = EmpAddress,
+        )
+        if status == "employee":
+            user_porfile.is_employee = True
+        user_porfile.save()
+        
+        messages.success(request, "Registration successfull!")
+        return redirect('user_login')        
+    return render(request, 'login_Register.html')
